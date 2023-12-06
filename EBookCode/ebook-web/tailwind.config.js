4d9808c7e9428360fccf4e7a3fcf89782844efb7
Xiao Jiong <jiongxnext0809@gmail.com>
2023-12-06 21:49:59 +0800

    配置默认字体

--- EBookCode/ebook-web/tailwind.config.js
+++ EBookCode/ebook-web/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+  darkMode: 'class',
 }
