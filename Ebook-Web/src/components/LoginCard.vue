bbec5faf7ffef82996c3da1d20948720c4502919
aqpower <jiongxnext0809@gmail.com>
2023-12-07 22:08:36 +0800

    windows 前端修改

--- Ebook-Web/src/components/LoginCard.vue
+++ Ebook-Web/src/components/LoginCard.vue
@@ -1,17 +1,8 @@
 <template>
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-60 w-auto" src="../assets/logo-text-nobg.png" alt="Your Company" />
-    <h2 class="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-      登录你的账户
-    </h2>
-  </div>
-
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" @submit.prevent="userLogin">
       <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-          >邮箱地址</label
-        >
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
         <div class="mt-2">
           <input
             id="email"
@@ -19,7 +10,7 @@
             type="email"
             autocomplete="email"
             required="true"
-            class="peer block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            class="peer block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-500 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
@@ -30,7 +21,7 @@
             >密码</label
           >
           <div class="text-sm">
-            <a href="#" class="font-semibold text-rose-400 hover:text-rose-300">忘记密码?</a>
+            <a href="#/resetPassword" class="font-semibold text-rose-400 hover:text-rose-300">忘记密码?</a>
           </div>
         </div>
         <div class="mt-2">
@@ -40,15 +31,14 @@
             type="password"
             autocomplete="current-password"
             required="true"
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-500 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
 
       <div>
         <button
-          type="submit"
-          class="flex w-full justify-center rounded-md bg-rose-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 f ocus-visible:outline-indigo-600"
+          class="flex w-full justify-center rounded-md bg-rose-400 active:bg-rose-200 hover:bg-rose-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
         >
           登录
         </button>
@@ -58,13 +48,29 @@
     <p class="mt-10 text-center text-sm text-gray-500">
       还没有账户🤔
       {{ ' ' }}
-      <a href="#" class="font-semibold leading-6 text-rose-400 hover:text-rose-300">点击注册！</a>
+      <a
+        @click="userLogin()"
+        class="font-semibold leading-6 text-rose-400 hover:text-rose-300 hover:cursor-pointer"
+        >点击注册！</a
+      >
     </p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { UserLoginApi } from '@/api/user'
 
+const obj = {
+  user_id: '12345678',
+  email: '123456@qq.com',
+  password: '123456'
+}
+
+const userLogin = () => {
+  UserLoginApi(obj).then((data) => {
+    console.log(data)
+  })
+}
 </script>
 
 <style scoped></style>
--- Ebook-Web/src/router/index.ts
+++ Ebook-Web/src/router/index.ts
@@ -1,24 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/login',
       name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/resetPassword',
       component: () => import('../views/LoginView.vue')
     }
   ]
