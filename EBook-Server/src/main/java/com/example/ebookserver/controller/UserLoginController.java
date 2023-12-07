54269d16d4767d7f2d8a3c13cbbb1a3e11b4bd0e
zhuxinr <1987564320@qq.com>
2023-12-07 19:50:28 +0800

    更新了部分server操作

---
+++ EBook-Server/src/main/java/com/example/ebookserver/controller/UserLoginController.java
@@ -0,0 +1,2 @@
+package com.example.ebookserver.controller;public class UserLoginController {
+}
--- EBook-Server/src/main/java/com/example/ebookserver/service/UserService.java
+++ EBook-Server/src/main/java/com/example/ebookserver/service/UserService.java
@@ -1,4 +1,8 @@
 package com.example.ebookserver.service.manage;
 
+import org.apache.ibatis.annotations.Mapper;
+
+
 public interface UserService {
+    void userLogin();
 }
