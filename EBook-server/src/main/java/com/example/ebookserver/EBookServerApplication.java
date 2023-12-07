20782e3277c53dabf0243a1a6ccca7d0a1ebafd9
zhuxinr <1987564320@qq.com>
2023-12-07 16:34:28 +0800

    修改项目名称

---
+++ EBook-server/src/main/java/com/example/ebookserver/EBookServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBookServerApplication.class, args);
+    }
+
+}
