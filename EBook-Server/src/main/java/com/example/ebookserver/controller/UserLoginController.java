a2b8fc3260cf8d2c7f01764537bec9af2f1447da
zhuxinr <1987564320@qq.com>
2023-12-07 21:47:31 +0800

    完成返回token令牌

--- EBook-Server/src/main/java/com/example/ebookserver/controller/UserLoginController.java
+++ EBook-Server/src/main/java/com/example/ebookserver/controller/UserLoginController.java
@@ -3,11 +3,15 @@ package com.example.ebookserver.controller;
 import com.example.ebookserver.pojo.Result;
 import com.example.ebookserver.pojo.User;
 import com.example.ebookserver.service.UserService;
+import com.example.ebookserver.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -18,6 +22,14 @@ public class UserLoginController {
     public Result user_login(@RequestBody User user){
         log.info("用户登录功能");
         User u = userService.userLogin(user);
-        return u != null ? Result.success():Result.error("登录失败");
+        if (u != null){
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id",u.getId());
+            claims.put("name",u.getUserName());
+            claims.put("email",u.getEmail());
+            String jwt = JwtUtils.generateJwt(claims);
+            return Result.success(jwt);
+        }
+        return Result.error("登录失败了哦");
     }
 }
