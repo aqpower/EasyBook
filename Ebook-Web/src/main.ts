cfb2be9940c3ade442ceb2e09a48950e6d20a172
Xiao Jiong <jiongxnext0809@gmail.com>
2023-12-09 12:25:47 +0800

    web中移除toast依赖

--- Ebook-Web/src/main.ts
+++ Ebook-Web/src/main.ts
@@ -6,7 +6,6 @@ import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
-import Toast, { type PluginOptions } from "vue-toastification";
 const app = createApp(App)
 
 const pinia = createPinia()
@@ -14,8 +13,4 @@ pinia.use(createPersistedState())
 app.use(pinia)
 app.use(router)
 
-const options: PluginOptions = {
-    // You can set your default options here
-};
-app.use(Toast, options);
 app.mount('#app')
