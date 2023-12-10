44c51c0899a254b5aca4a77bd306518964a6e5ac
wzb2233 <2010568462@qq.com>
2023-12-11 00:29:00 +0800

    创建管理员登录接口

--- EBook-Server/src/main/java/com/example/ebookserver/pojo/Admin.java
+++ EBook-Server/src/main/java/com/example/ebookserver/pojo/Admin.java
@@ -1,5 +1,7 @@
 package com.example.ebookserver.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Admin {
+    @TableId(type = IdType.AUTO)
     Integer id;
     String name;
     String password;
--- EBook-Server/src/main/java/com/example/ebookserver/service/manage/AdminServiceImpl.java
+++ EBook-Server/src/main/java/com/example/ebookserver/service/manage/AdminServiceImpl.java
@@ -1,13 +1,19 @@
 package com.example.ebookserver.service.manage;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.ebookserver.mapper.AdminMapper;
 import com.example.ebookserver.pojo.Admin;
+import com.example.ebookserver.pojo.LoginData;
 import com.example.ebookserver.service.AdminService;
+import com.example.ebookserver.utils.JwtUtils;
 import com.example.ebookserver.utils.MD5Util;
 import jdk.jpackage.internal.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Random;
 
 @Service
@@ -16,18 +22,31 @@ public class AdminServiceImpl implements AdminService {
     private AdminMapper adminMapper;
 
     @Override
-    public Admin adminLogin(Admin admin) {
+    public LoginData adminLogin(Admin admin) {
+        Map<String, Object> claims = new HashMap<>();
+        LoginData loginData = new LoginData();
         String password = admin.getPassword();
+        System.out.println(password);
+        claims.put("id",admin.getId());
+        claims.put("name",admin.getName());
+        loginData.setId(admin.getId());
+        String jwt = JwtUtils.generateJwt(claims);
+        loginData.setToken(jwt);
 
-        if(admin.getId() != null) {
-            Integer id = admin.getId();
-            return adminMapper.getById(id, password);
-        } else if (admin.getName() != null) {
-            String username = admin.getName();
-            return adminMapper.getByUsername(username, password);
-        } else {
-            return null;
+        LambdaQueryWrapper<Admin> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Admin::getId, admin.getId());
+        Admin one = adminMapper.selectOne(wrapper);
+        if (one == null) {
+            loginData.setCode(1);
+            return loginData;
         }
+       loginData.setName(one.getName());
+        if (!MD5Util.encode(admin.getPassword()).equals(one.getPassword())) {
+            loginData.setCode(2);
+            return loginData;
+        }
+        loginData.setCode(3);
+        return loginData;
     }
     @Override
     public Admin createAdmin(Admin admin) {
@@ -35,7 +54,7 @@ public class AdminServiceImpl implements AdminService {
         admin.setRole(role);
         //TODO:自定义密码
         String password = this.generatePassword();
-
+//        password = MD5Util.encode(password);
         admin.setPassword(password);
         adminMapper.insertAdmin(admin.getName(), MD5Util.encode(password), (short) 2);
         return admin;
