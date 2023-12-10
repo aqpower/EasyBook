68d9f05f325a1f888118903da5607589589ea007
zhuxinr <1987564320@qq.com>
2023-12-10 20:36:03 +0800

    完成主页帖子分页展示功能

---
+++ EBook-Server/src/main/java/com/example/ebookserver/pojo/PostShow.java
@@ -0,0 +1,24 @@
+package com.example.ebookserver.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostShow {
+    private Integer id; //帖子id  post
+    private short avatar;  //发帖人头像  user
+    private String name;  //发帖人昵称   user
+    private String title; //标题   post
+    private String contentText; //文字内容 post
+    private short color;  //帖子颜色  post
+    private String lyrics;  //歌词  post
+    private List<String> url;//图片链接集合 image
+    private Integer likeNum;  //喜欢数量  post
+    private Integer collectionNum;  //收藏数量 post
+    private String ip;  //ip地址  post
+}
